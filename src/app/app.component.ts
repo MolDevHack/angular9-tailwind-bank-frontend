b671bb4111d15f77b5d1cc48d38a280d75e3d3d5
timeshift <akhmedov@infosystems.uz>
2021-06-04 22:56:35 +0500

    gh pages

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'finance-app';
+
+  ngOnInit() {
+    self.addEventListener('hashchange', () => {
+      parent.postMessage(['hashchange', location.href]
+        , '*')
+    })
+
+  }
 }
