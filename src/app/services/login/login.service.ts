e4f952a5c6a801649d4160b0caeac787eda26716
timeshift <akhmedov@infosystems.uz>
2021-06-05 21:33:31 +0500

    added theme and login to localstorage

--- src/app/services/login/login.service.ts
+++ src/app/services/login/login.service.ts
@@ -29,6 +29,10 @@ export class LoginService {
     if (isLoggedIn != undefined) {
       this.isLoggedIn = isLoggedIn == 'true' ? true : false
     }
+    const user = localStorage.getItem('user');
+    if(user != undefined) {
+      this.userSubject.next(JSON.parse(user))
+    }
 
   }
 
@@ -41,6 +45,11 @@ export class LoginService {
     this.errorSubject.next(null);
     this.isLoggedIn = true;
     localStorage.setItem('isLoggedIn', this.isLoggedIn.toString());
+    localStorage.setItem('user',JSON.stringify({
+      Accounts: [
+        { Balance: 1123 }
+      ]
+    }));
     this.router.navigateByUrl('/dashboard');
     // this.http.post(this.url, { Username, Password }, httpOptions).toPromise().then((res: any) => {
     //   if (res.Data && res.Data.length) {
